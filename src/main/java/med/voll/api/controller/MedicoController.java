a80656746cbf139069630c55ff40275fdbbaa924
alyssonlemes <alyssondiamantino@gmail.com>
2024-05-27 21:01:53 -0300

    MedicoService Feature v1

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,8 +1,10 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
-import med.voll.api.endereco.Endereco;
-import med.voll.api.medico.*;
+import med.voll.api.medico.MedicoService;
+import med.voll.api.medico.dto.DadosAtualizacaoMedico;
+import med.voll.api.medico.dto.DadosCadastroMedico;
+import med.voll.api.medico.dto.DadosListagemMedico;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -10,38 +12,33 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/medicos")
 public class MedicoController {
 
     @Autowired
-    private MedicoRepository repository;
+    private MedicoService medicoService;
 
     @PostMapping
     @Transactional
-    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados){
-
-        repository.save(new Medico(dados)); //salva no banco de dados os dados do cadastro do médico através da classe repository
+    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
+        medicoService.cadastrar(dados);
     }
 
     @GetMapping
-    public Page<DadosListagemMedico> listar (@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
-        return repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new); //retorna uma page com as informações da listagem de medicos
+    public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
+        return medicoService.listar(paginacao);
     }
 
     @PutMapping
     @Transactional
-    public void atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados){
-        var medico = repository.getReferenceById(dados.id()); //carrega a variavel medico pelo id do banco de dados
-        medico.atualizarInformacoes(dados);
+    public void atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados) {
+        medicoService.update(dados);
     }
 
-    @DeleteMapping("/{id}") //exemplo de parametro dinamico que chegará através da url na requisição
+    @DeleteMapping("/{id}")
     @Transactional
-    public void excluir(@PathVariable Long id){ //o PathVariable avisa que o parÂmetro long id vem do parametro dinamico definido anteriormente
-        var medico = repository.getReferenceById(id); //carrega a variavel medico pelo id do banco de dados
-        medico.excluir();
+    public void excluir(@PathVariable Long id) {
+        medicoService.excluir(id);
     }
 }
